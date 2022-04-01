e36dc3aac9d302f48d975984e7f52864e86f5250
Jayeen28 <jayeen6783@gmail.com>
2022-04-01 22:35:00 +0600

    Pin validation done

--- atm.c
+++ atm.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){
+/**
+ * @brief This function is used for checking the pin user has inserted.
+ * If user has inserted wrong pin then it calls itself again.
+ */
+void checkPin(){
+	int pin_number = 1234,pin_input;
+	scanf("%d",&pin_input);
+	if(pin_input != pin_number){
+		printf("Wrong pin. Please try again: ");
+		checkPin();
+	}
+};
 
+int main(){
+	printf("Enter your pin: ");
+	checkPin();
 	return 0;
 };
